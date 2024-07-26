8baed90c5eeffdf417928b717ba8680d261d04df
KirasSsin <152463849+KirasSsin@users.noreply.github.com>
2024-07-26 15:09:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
-#  Copyright (c) ChernV (@otter18), 2021.
-
 import os
 import random
+import config
+import telebot # pip install telebot
 
 from setup import bot, logger
 from webhook import app
+from telebot import types # pip install pyTelegramBotAPI
+
+bot = telebot.TeleBot(config.token)
+
 
 # --------------- dialog params -------------------
 dialog = {
     'hello': {
         'in': ['привет', 'hello', 'hi', 'privet', 'hey'],
-        'out': ['Приветствую', 'Здравствуйте', 'Привет!']
-    },
-    'how r u': {
-        'in': ['как дела', 'как ты', 'how are you', 'дела', 'how is it going'],
-        'out': ['Хорошо', 'Отлично', 'Good. And how are u?']
-    },
-    'name': {
-        'in': ['зовут', 'name', 'имя'],
-        'out': [
-            'Я telegram-template-bot',
-            'Я бот шаблон, но ты можешь звать меня в свой проект',
-            'Это секрет. Используй команду /help, чтобы узнать'
-        ]
-    }
-}
+        'out': ['Здравствуйте']
+             } 
+         }
 
 
 # --------------- bot -------------------
 @bot.message_handler(commands=['help', 'start'])
 def say_welcome(message):
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used /start or /help')
+    logger.info(f'@{message.from_user.username} ({message.chat.id}) used /start or /help')
     bot.send_message(
         message.chat.id,
-        '<b>Hello! This is a telegram bot template written by <a href="https://github.com/otter18">otter18</a></b>',
+        '<b>Hello! This is a telegram bot template written by <a href="https://gitlab.goodsforecast.ru/myshak_kirill/TG_Bot">TGBot</a></b>',
         parse_mode='html'
     )
 
@@ -41,12 +33,12 @@ def say_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo(message):
     for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
+        if any(e in message.text.lower() for e in resp['in']):
+            logger.info(f'@{message.from_user.username} ({message.chat.id}) used {t}:\n\n%s', message.text)
             bot.send_message(message.chat.id, random.choice(resp['out']))
             return
 
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
+    logger.info(f'@{message.from_user.username} ({message.chat.id}) used echo:\n\n%s', message.text)
     bot.send_message(message.chat.id, message.text)
 
 
