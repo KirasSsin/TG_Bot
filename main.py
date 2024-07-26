cd686aae722ea7d5bcaca57d715ea58b8d776b57
KirasSsin <152463849+KirasSsin@users.noreply.github.com>
2024-07-26 15:16:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,94 @@
-import os
-import random
-import config
-import telebot # pip install telebot
+import telebot
+from config import TOKEN
 
-from setup import bot, logger
-from webhook import app
-from telebot import types # pip install pyTelegramBotAPI
-
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(7397871713:AAHi28Ocb2a4pDjW1S_97VHfBgYJ6gMzEkU)
 
 
-# --------------- dialog params -------------------
-dialog = {
-    'hello': {
-        'in': ['привет', 'hello', 'hi', 'privet', 'hey'],
-        'out': ['Здравствуйте']
-             } 
-         }
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    button_save = telebot.types.InlineKeyboardButton(
+        text="Написать в поддержку")
+    keyboard.add(button_save)
+    bot.send_message(chat_id,
+                     'Добро пожаловать в бота сбора обратной связи',
+                     reply_markup=keyboard)
+    with open('feedback.jpeg', 'rb') as file:
+        photo = file.read()
+    bot.send_photo(chat_id, photo)
 
 
-# --------------- bot -------------------
-@bot.message_handler(commands=['help', 'start'])
-def say_welcome(message):
-    logger.info(f'@{message.from_user.username} ({message.chat.id}) used /start or /help')
-    bot.send_message(
-        message.chat.id,
-        '<b>Hello! This is a telegram bot template written by <a href="https://gitlab.goodsforecast.ru/myshak_kirill/TG_Bot">TGBot</a></b>',
-        parse_mode='html'
-    )
+@bot.message_handler(
+    func=lambda message: message.text == 'Написать в поддержку')
+def write_to_support(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Введите своё имя')
+    users[chat_id] = {}
+    bot.register_next_step_handler(message, save_username)
 
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    for t, resp in dialog.items():
-        if any(e in message.text.lower() for e in resp['in']):
-            logger.info(f'@{message.from_user.username} ({message.chat.id}) used {t}:\n\n%s', message.text)
-            bot.send_message(message.chat.id, random.choice(resp['out']))
-            return
+def save_username(message):
+    chat_id = message.chat.id
+    name = message.text
+    users[chat_id]['name'] = name
+    bot.send_message(chat_id, f'Отлично, {name}. Теперь укажи свою фамилию')
+    bot.register_next_step_handler(message, save_surname)
 
-    logger.info(f'@{message.from_user.username} ({message.chat.id}) used echo:\n\n%s', message.text)
-    bot.send_message(message.chat.id, message.text)
+
+def save_surname(message):
+    chat_id = message.chat.id
+    surname = message.text
+    users[chat_id]['surname'] = surname
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button_save = telebot.types.InlineKeyboardButton(text="Сохранить",
+                                                     callback_data='save_data')
+    button_change = telebot.types.InlineKeyboardButton(text="Изменить",
+                                                     callback_data='change_data')
+    keyboard.add(button_save, button_change)
+
+    bot.send_message(chat_id, f'Сохранить данные?', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['who_i'])
+def who_i(message):
+    chat_id = message.chat.id
+    name = users[chat_id]['name']
+    surname = users[chat_id]['surname']
+    bot.send_message(chat_id, f'Вы: {name} {surname}')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'save_data')
+def save_btn(call):
+    message = call.message
+    chat_id = message.chat.id
+    message_id = message.message_id
+    bot.answer_callback_query(call.id, text="Данные сохранены")
+    bot.delete_message(chat_id=chat_id, message_id=message_id)
+    bot.send_message(chat_id, 'Введите текст отзыва: ')
+    bot.register_next_step_handler(message, send_feedback_administrators)
+
+
+def send_feedback_administrators(message):
+    feedback = message.text
+    user = users[message.chat.id]
+    name = user['name']
+    surname = user['surname']
+    for admin_chat_id in administrators:
+        bot.send_message(admin_chat_id,
+                         f'{surname} {name} оставил отзыв: {feedback}')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'change_data')
+def save_btn(call):
+ message = call.message
+    chat_id = message.chat.id
+    message_id = message.message_id
+    bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                          text='Изменение данных!')
+    write_to_support(message)
 
 
 if __name__ == '__main__':
-    if os.environ.get("IS_PRODUCTION", "False") == "True":
-        app.run()
-    else:
-        bot.infinity_polling()
+    print('Бот запущен!')
+    bot.infinity_polling()
